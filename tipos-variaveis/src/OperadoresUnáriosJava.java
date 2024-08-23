f632e30dc744015a0839bc29a3cac5b37f4455bc
lavilads <leandrolavila16@gmail.com>
2024-08-23 19:28:51 -0300

    add exemplo de operadoeres unirários

---
+++ tipos-variaveis/src/OperadoresUnáriosJava.java
@@ -0,0 +1,16 @@
+public class OperadoresUnáriosJava {
+    public static void main(String[] Args) throws Exception {
+        // Operadores Unários
+        int numero = 5;
+
+        System.out.println(+numero);    // Saída: 5
+        System.out.println(-numero);    // Saída: -5
+        System.out.println(numero++);   // Saída: 5 (Sufixal)
+        System.out.println(++numero);   // Saída: 7 (Prefixal)
+        System.out.println(numero--);   // Saída: 7 (Sufixal)
+        System.out.println(--numero);   // Saída: 5 (Prefixal)
+        System.out.println(-(-numero)); // Saída: 5 
+
+        System.out.println(!true);      // Saída: false
+    }
+}
