1c2b9b18f02b932ca278ea67eca09963dcd4e48e
lavilads <leandrolavila16@gmail.com>
2024-08-23 18:57:41 -0300

    adicionando os tipos do Java

---
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -0,0 +1,30 @@
+public class TipoVariaveis {
+    public static void main(String[] args) throws Exception {
+        // Tipos Primitivos
+        char letra = 'a';                // [1 byte]
+        byte numeroMuitoPequeno = 127;   // [1 Byte]
+        short numeroPequeno = 32767;     // [2 Bytes]
+        int numeroMedio = 2147483647;    // [4 Bytes]]
+        long numeroGrande;               // [8 Bytes]
+        float temperatura = 1.87F;       // [4 Byte]
+        double comprimento = 1.87;       // [8 Bytes]
+        boolean proposicao = false;      // [1 byte]
+
+        final double VALOR_DE_PI = 3.14; // Declara uma Constante
+
+        // Classe 
+        String nome = "Leandro";
+
+        // Classe Wraper
+        Character wraperChar;
+        Byte wraperByte;
+        Short wraperShort;
+        Integer wraperInteger;
+        Long wraperLong;
+        Float wraperFloat;
+        Double wraperDouble;
+
+
+        System.out.println("Hello, World!");
+    }
+}
