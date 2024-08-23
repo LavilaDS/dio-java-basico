dcd65b92c976e7aacbc16836db56db193ef2fe08
lavilads <leandrolavila16@gmail.com>
2024-08-23 20:23:00 -0300

    adicionando o arquivo OperadoresRelacionais.java

---
+++ java-basico/src/OperadoresRelacionais.java
@@ -0,0 +1,18 @@
+public class OperadoresRelacionais {
+    public static void main(String[] args) throws Exception {
+        // Operadores Relacionais
+
+        int num1 = 5;
+        int num2 = 10;
+
+        System.out.println(5 != 10);   // Saída: true
+        System.out.println(5 > 10);   // Saída: false
+        System.out.println(5 == 10);   // Saída: false
+        System.out.println(5 >= 10);   // Saída: false
+        System.out.println(5 < 10);    // Saída: true
+        System.out.println(5 <= 10);   // Saída: true
+        
+        
+
+    }
+}
